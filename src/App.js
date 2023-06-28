4302b6a253b1d8f30e9b8e2749e16aece162b67e
kiran97171 <120737308+kiran97171@users.noreply.github.com>
2023-06-28 22:44:16 +0530

    not

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import RoutesFile from "./Components/RoutersFile";
+import "./style.css";
+
+function App() {
+  return (
+    <>
+   <div>
+    <RoutesFile/>
+   </div>
+    </>
+  );
+}
+
+export default App;
